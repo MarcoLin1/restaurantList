e23109154c1278bc8d7bc553716361a7da5ad010
Marco Lin <pohanlin0605@gmail.com>
2021-04-24 17:57:58 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const app = express()
 const port = 3000
 const exhbs = require('express-handlebars')
@@ -12,6 +13,13 @@ require('./config/mongoose')
 // setting handlebars
 app.engine('hbs', exhbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+// setting session 
+app.use(session({
+  secret: 'secretId',
+  resave: false,
+  saveUninitialized: true
+}))
 
 // browse app will first load the files and setting 
 app.use(express.static('public'))

5548726162ad5fdd23f692acfc69ecd32b6070fe
Marco Lin <pohanlin0605@gmail.com>
2021-04-25 13:46:26 +0800

    modify restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,23 +1,60 @@
 const Restaurant = require('../restaurant')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const restaurantJson = require('./restaurant.json')
 const db = require('../../config/mongoose')
+const bcrypt = require('bcryptjs')
+const User = require('../user')
+
+const SEED_USER = [{
+  name: 'user1',
+  email: 'user1@example.com',
+  password: '12345678'
+}, {
+  name: 'user2',
+  email: 'user2@example.com',
+  password: '12345678'
+}]
+
 
 db.once('open', () => {
-  console.log('mongodb connected!')
-  for (let i = 0; i < restaurantJson.results.length; i++) {
-    Restaurant.create({
-      name: restaurantJson.results[i].name,
-      name_en: restaurantJson.results[i].name_en,
-      category: restaurantJson.results[i].category,
-      image: restaurantJson.results[i].image,
-      location: restaurantJson.results[i].location,
-      phone: restaurantJson.results[i].phone,
-      google_map: restaurantJson.results[i].google_map,
-      googleMap: restaurantJson.results[i].googleMap,
-      rating: restaurantJson.results[i].rating,
-      description: restaurantJson.results[i].description
-    })
-  }
-  console.log('completed')
+  SEED_USER.forEach((user, index) => {
+    bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(user.password, salt))
+      .then(hash => User.create({
+        name: user.name,
+        email: user.email,
+        password: hash
+      }))
+      .then(seedUser => {
+        const userId = seedUser._id
+        const restaurantList = restaurantJson.results
+
+        return Promise.all(Array.from(
+          { length: 3 },
+          (_, i) => Restaurant.create({
+            name: restaurantList[i + (index * 3)].name,
+            name_en: restaurantList[i + (index * 3)].name_en,
+            category: restaurantList[i + (index * 3)].category,
+            image: restaurantList[i + (index * 3)].image,
+            location: restaurantList[i + (index * 3)].location,
+            google_map: restaurantList[i + (index * 3)].google_map,
+            googleMap: restaurantList[i + (index * 3)].googleMap,
+            rating: restaurantList[i + (index * 3)].rating,
+            description: restaurantList[i + (index * 3)].description,
+            userId: userId
+          })
+        ))
+      })
+      .then(() => {
+        console.log('done')
+        process.exit()
+      })
+      .catch(error => console.log(error))
+  })
 })
 
